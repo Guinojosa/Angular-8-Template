30c72b9c9c4e32be4875e269c14669f7c90b1f4a
Guilherme Santos <guilherme.santos@fortaleza.ce.gov.br>
2019-10-09 14:05:40 -0300

    Adjust The RandomNumbers in feedbacks

--- src/app/Components/feedbacks/feedbacks.component.ts
+++ src/app/Components/feedbacks/feedbacks.component.ts
@@ -8,11 +8,13 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FeedbacksComponent implements OnInit {
-  array: number[] = [1, 8, 4, 10, 9, 8, 5 , 9, 7, 8, 7, 7, 7, 7, 7, 7, 10, 9, 8, 10];
+  array: number[];
   typeNps: number;
   breakpoint: number;
   // tslint:disable-next-line: variable-name
-  constructor(private _toast: ToastService) { }
+  constructor(private _toast: ToastService) {
+    this.randomNumbers();
+  }
 
   ngOnInit() {
     this.breakpoint = (window.innerWidth <= 740) ? 1 : 3;
@@ -24,7 +26,12 @@ export class FeedbacksComponent implements OnInit {
 
   showToast(value) {
     const typeNps = value >= 0 && value  <= 6  ? 'Detractor' : value  >= 7 && value  <= 8  ? 'Neutral' : 'Promoter';
-    this._toast.toastAlert('Type: ' + typeNps, 'success');
+    this._toast.toastAlert('Type: ' + typeNps, 'info');
   }
 
+  randomNumbers() {
+    this.array = Array.from({length: 12}).map(i => {
+      return Math.floor(Math.random() * (10 - 0) + 0);
+    });
+  }
 }
