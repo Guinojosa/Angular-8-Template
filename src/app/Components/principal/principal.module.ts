5d8e6e63d88fd9369f45f9a4aef96abaa0dd4807
Guilherme Santos <guilherme.santos@fortaleza.ce.gov.br>
2019-10-10 13:42:52 -0300

    Added the SideNav

--- src/app/Components/principal/principal.module.ts
+++ src/app/Components/principal/principal.module.ts
@@ -1,9 +1,12 @@
+import { SharedModule } from './../../Shared/shared.module';
 import { PrincipalComponent } from './principal.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PrincipalRoutingModule } from './principal-routing.module';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 
 @NgModule({
@@ -11,7 +14,10 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   imports: [
     CommonModule,
     PrincipalRoutingModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatExpansionModule,
+    SharedModule
   ]
 })
 export class PrincipalModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     CommonModule,
     AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
